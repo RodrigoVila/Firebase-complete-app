d865aa88ad9a4d1f22b62c287cf132ba69152354
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2020-01-24 17:08:50 -0300

    Styles added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard';
-import Projectdetails from './components/projects/ProjectDetails';
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import Projectdetails from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject"
 
 function App() {
   return (
@@ -11,8 +14,11 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/' component={Dashboard}/>
-          <Route path='/project/:id' component={Projectdetails}/>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={Projectdetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
